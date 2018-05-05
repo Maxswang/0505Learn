895579ad9457c1a1fef008e9e39d530b6d6480d2
wgm <wang.gm63@gmail.com>
2018-05-05 22:33:46 +0800

    增加了头文件

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
 int main()
 {
     const int a = 10;
-    const int &b = a;
+   const int &b = a;
 
     return 0;
 }
